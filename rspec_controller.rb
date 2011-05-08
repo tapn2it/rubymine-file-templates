20ba637089f5117a50db152eb117829ccf66be02
Victor Christensen <vc@elctech.com>
2011-05-07 17:28:08 -0700

    initial code

---
+++ rspec_controller.rb
@@ -0,0 +1,11 @@
+#parse("Ruby File Header.rb")
+require "rspec"
+
+describe ${Behaviour_Name} do
+
+  it "should ${it_should}" do
+
+    #To change this template use File | Settings | File Templates.
+    true.should == false
+  end
+end
